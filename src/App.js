fd62aa496cb5902dff0b5edc59bea74d0625ee43
ramprasad0723 <68224325+ramprasad0723@users.noreply.github.com>
2020-07-13 15:50:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import Appbar from './component/Appbar'
+import  Login from './login/login'
+import './login/login.css';
+class App extends Component {
+    render() {
+        return (
+               <Router>
+            <div className = "App" >
+           
+            <Route path="/" exact component={Login}/>
+            <Route path="/appbar" component={Appbar}/>
+
+            </div>
+            </Router>
+        )
+    }
+}
+
+export default App
